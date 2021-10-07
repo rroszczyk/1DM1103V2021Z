b929144a193fdb27149bb1e8c5b047f8496e97db
Radosław Roszczyk <roszczyr@baltic1a.iem.pw.edu.pl>
2021-10-07 09:42:25 +0200

    dodanie przykładów S01

---
+++ S01/pr2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+   int i;
+   printf("Program działa i wypisuje parametry wywołania\n");
+
+   for (i = 0; i < argc; ++i)
+   {  
+      printf("%i: %s \n", i, argv[i]);
+   }
+   
+   return 1;
+}
